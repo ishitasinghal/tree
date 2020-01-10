80c95b7ea6a8bc40d450caf6f2f9208b71b42e3d
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2020-01-10 18:37:00 +0530

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -6,4 +6,13 @@ class Tree:
     def insert(self, data):
         if(self.data):
             if(data<self.data):
-                if(self.data
+                if(self.data is None):    #if there is no data on the left node
+                    self.left = Tree(data)
+                else:
+                    self.left.insert(data)
+            elif(data>self.data):
+                if(self.data is None):    #if there is no data on the right node
+                    self.right = Tree(data)
+                else:
+                    self.right.insert(data)
+                
