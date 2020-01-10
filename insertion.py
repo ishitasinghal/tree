350d4fbea6686b358ea96df59b7707d414503b3b
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2020-01-10 18:33:19 +0530

    Create insertion.py

---
+++ insertion.py
@@ -0,0 +1,9 @@
+class Tree:
+    def __init__(self, data):
+        self.right = None
+        self.data = data
+        self.right = None
+    def insert(self, data):
+        if(self.data):
+            if(data<self.data):
+                if(self.data
