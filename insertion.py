f854e9604be3fc104ef70cd0a71c54755b1b1a7e
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2020-01-11 16:02:37 +0530

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -3,18 +3,18 @@ class Tree:
         self.right = None
         self.data = data
         self.left = None
-    def insert(self, data):
+    def inser(self, data):
         if(self.data):
             if(data<self.data):
                 if(self.data is None):    #if there is no data on the left node
                     self.left = Tree(data)
                 else:
-                    self.left.insert(data)
+                    self.left.inser(data)
             elif(data>self.data):
                 if(self.right is None):    #if there is no data on the right node
                     self.right = Tree(data)
                 else:
-                    self.right.insert(data)
+                    self.right.inser(data)
         else:
             self.data = data
     def printdata(self):
@@ -25,11 +25,11 @@ class Tree:
             self.right.printdata()
 
 root = Tree(31)
-root.insert(4)
-root.insert(8)
-root.insert(12)
-root.insert(16)
-root.insert(0)
-root.insert(36)
-root.insert(28)
+root.inser(4)
+root.inser(8)
+root.inser(12)
+root.inser(16)
+root.inser(0)
+root.inser(36)
+root.inser(28)
 print(root.printdata())
