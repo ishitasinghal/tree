e614976b4bc897e0b3df76129652ae480725afd0
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2020-01-16 19:43:23 +0530

    Update traversal.py

--- traversal.py
+++ traversal.py
@@ -7,3 +7,12 @@ class Node:
         if(self.data):
             
         
+##function     
+def preOrder(root):
+    list1 = []
+    if(root):
+        list1.append(root.info)
+        if(root.left):
+            preOrder(root.left)
+        else:
+            preOrder(root.right)
